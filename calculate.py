bb8e0d38ef622d1576f8b2ac78fa42f5ed84cd37
superPershing <north.wind@qq.com>
2018-04-25 20:32:25 +0800

    finish percentage

--- calculate.py
+++ calculate.py
@@ -2,7 +2,16 @@ import pandas as pd
 import sys
 
 # filename = "WedApr251720092018.csv"
-filename = sys.argv[1]
-df = pd.read_csv(filename, delim_whitespace=True)
-success_df = df.loc[df['status']=="success"]
-print(success_df[["run_time", "route_length"]].mean())
+def print_info():
+    filename = sys.argv[1]
+    df = pd.read_csv(filename, delim_whitespace=True)
+    success_df = df.loc[df['status']=="success"]
+    print("Statistics information")
+    print(success_df[["run_time", "route_length"]].mean())
+
+    print("Total run " + str(len(df)) + " rounds")
+    print("Success " + str(len(success_df)) + " rounds " + str((len(success_df) / len(df))*100) + "%")
+    print("Fail " + str(len(df) - len(success_df)) + " rounds " + str(((len(df) - len(success_df)) / len(df))*100) + "%")
+
+if __name__ == "__main__":
+    print_info()
